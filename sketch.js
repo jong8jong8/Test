80a5363f2f2e6d9cbb599432a78788598aa4e1e3
jong8jong8 <jong8jong8@gmail.com>
2023-02-11 09:25:55 +0900

    source

--- sketch.js
+++ sketch.js
@@ -1,16 +1,12 @@
+let song;
+
+function preload() {
+    song = loadSound("The Columbians - Just Like A Rainbow.mp3");
+}
+
 function setup() {
-    createCanvas(600, 400);
+    console.log("start");
+    song.play();
+    
 }
-
-function draw() {
-    background(0);
-    strokeWeight(4);
-    stroke(255);
-
-    for (let x = 0; x <= mouseX; x += 50) {
-        for (let y = 0; y <= height; y += 50) {
-            fill(random(255), 0, random(255));
-            ellipse(x, y, 25, 25);
-        }
-    }   
-}
+ 
