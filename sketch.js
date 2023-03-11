e45b0e9722db027c82ead5a1009f53dddb4b47c4
jong8jong8 <jong8jong8@gmail.com>
2023-03-11 11:20:38 +0900

    image & speech

--- sketch.js
+++ sketch.js
@@ -1,20 +1,43 @@
+
+
+let mobilenet;
+let bird;
 let speech;
 
+
 function preload() {
-  // speech = new p5.Speech();
+    mobilenet = ml5.imageClassifier('MobileNet', modelReady);
 }
 
 function setup() {
-  noCanvas();
-  speech = new p5.Speech();
+    createCanvas(640, 480);
+    bird = createImg('bird.jpg', imageReady);
+    bird.hide();
+    background(0);
+    speech = new p5.Speech();
+    
 }
 
-function keyPressed() {
-  if (key == 'e') {
-    speech.speak('Hello world'); // English
-  } else if (key == 'k') {
-    speech.speak('안녕'); // 한국어
-  } else if (key == 'n') {
-    speech.speak('18'); // 수
-  }
+function modelReady() {
+    console.log('Model is ready!');
+    mobilenet.classify(bird, gotResults);
 }
+
+function gotResults(error, results) { 
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(results);
+        let label = results[0].label;
+        let confidence = results[0].confidence;
+        createP(label);
+        createP(confidence);
+        speech.speak(`This would be ${label}`);
+    }
+}
+
+function imageReady() {
+    console.log('image is ready!');
+    image(bird, 0, 0, width, height); 
+}
+ 
